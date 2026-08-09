70b92b58a8239facdac000a31d21208239aadc80
Anil Jala <jalaanil@gmail.com>
2026-08-09 13:36:37 +0200

    changes to test docker

--- thymeleaf-demo/src/main/java/com/example/demo/controller/HomeController.java
+++ thymeleaf-demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
 
-        model.addAttribute("title", "Welcome to Thymeleaf");
+        model.addAttribute("title", "Welcome to Thymeleaf - 09.08.2026");
         model.addAttribute("message", "This text comes from the controller.");
 
         return "index";
